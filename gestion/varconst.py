a951f1cb8363ad7892c9eb693a4f6b72a404b290
cidesena <cidesenadev@gmail.com>
2025-02-17 13:08:30 -0500

    gestion de datos con python

---
+++ gestion/varconst.py
@@ -0,0 +1,20 @@
+
+#DEBILMENTE TIPADO
+print("oh gloria inmarcesible", end="---")
+#print()
+print("oh júbilo inmortal", end="")
+#print()
+print("en surcos de dolores")
+#print()
+print("el bien germina allá")
+
+x=100
+print(x)
+print(type(x))
+x=10.5
+print(x)
+print(type(x))
+x="amamos programar"
+print(x)
+print(type(x))
+
